5ee76672ec89ba3952da054b1f01196f1f0172cb
solracss <herekespanol@gmail.com>
2023-01-02 00:27:01 +0100

    Implement method for showing number for provided contact name

--- PhoneBook/PhoneBook.cs
+++ PhoneBook/PhoneBook.cs
@@ -33,5 +33,25 @@
             }
         }
 
+        public void ShowNumberForContact(string contactName)
+        {
+            if (string.IsNullOrEmpty(contactName))
+            {
+                Console.WriteLine("Please provide name");
+                return;
+            }
+
+            bool notFound = true;
+            foreach (var contact in Contacts.Where(c => c.Value.Name == contactName))
+            {
+                Console.WriteLine($"Phone number for {contactName} is {contact.Key}");
+                notFound = false;
+            }
+
+            if (notFound)
+            {
+                Console.WriteLine($"No contact for {contactName}");
+            }
+        }
     }
 }
