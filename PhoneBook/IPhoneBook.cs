58b469e224b04119568140db5079c8b7c84bac0b
solracss <herekespanol@gmail.com>
2023-01-01 10:44:43 +0100

    Add interface for phonebook class

--- PhoneBook/IPhoneBook.cs
+++ PhoneBook/IPhoneBook.cs
@@ -2,5 +2,12 @@
 {
     internal interface IPhoneBook
     {
+        void ShowAllContacts();
+
+        void AddContact(string number, Contact contact);
+
+        void ShowNumberForContact(string contactName);
+
+        void ShowContactName(string contactName);
     }
 }
