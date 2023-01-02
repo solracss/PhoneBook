17246c723f5187c8f7ffd799079bab3aded5ece2
solracss <herekespanol@gmail.com>
2023-01-02 18:32:27 +0100

    Move UI to separate class

---
+++ PhoneBook/UserInterface.cs
@@ -0,0 +1,41 @@
+﻿namespace PhoneBook
+{
+    internal static class UserInterface
+    {
+        internal static void ShowMainMenu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1: Add new contact");
+            Console.WriteLine("2: Show all contacts");
+            Console.WriteLine("3: Show number for contact");
+            Console.WriteLine("4: Show contact name for number");
+            Console.WriteLine("5: Clear screen");
+            Console.WriteLine("6: Exit Application");
+            Console.WriteLine();
+            Console.Write("Choose your action: ");
+        }
+
+        internal static string AskForname()
+        {
+            Console.Write("Please provide Name: ");
+            return Console.ReadLine();
+        }
+
+        internal static string AskForNumber()
+        {
+            Console.Write("Please provide Number: ");
+            return Console.ReadLine();
+        }
+
+        internal static void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        internal static void InvalidOperationMessage()
+        {
+            Console.WriteLine("Invalid operation");
+            Console.WriteLine();
+        }
+    }
+}
